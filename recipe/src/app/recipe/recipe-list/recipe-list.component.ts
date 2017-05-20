aee70ef86cbced37ba21aa0b0539b588db9b1648
Nino <nino.zumstein@gmail.com>
2017-05-21 00:33:00 +0200

    recipe

---
+++ recipe/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styles: []
+})
+export class RecipeListComponent implements OnInit {
+
+@Output() recipeSelected = new EventEmitter<Recipe>();  
+recipe = new Recipe('Dummy', 'hallo', 'http://i.telegraph.co.uk/multimedia/archive/03491/Vladimir_Putin_1_3491835k.jpg') 
+selectedRecipe: Recipe;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
+
+}
