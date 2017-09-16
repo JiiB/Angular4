17acd7134868b454edc2d30fa3a1480c5add9935
Nino <nino.zumstein@gmail.com>
2017-09-17 01:34:48 +0200

    checklist project created, added material & firebase

--- Checklist/src/app/app.component.ts
+++ Checklist/src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, AfterViewInit} from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import {MdSidenav} from '@angular/material';
+import {database} from "firebase/app";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit{
+  @ViewChild('start') sidenav: MdSidenav;
+
+  user: Observable<firebase.User>;
+  userInfo: any;
+  items: FirebaseListObservable<any[]>;
+  constructor(public afAuth: AngularFireAuth, public db: AngularFireDatabase) {
+    this.user = afAuth.authState;
+    this.items = db.list('items');
+
+    this.user.subscribe((auth) => {
+      if(auth != null){
+        console.log(auth);
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    if (window.innerWidth <= 799) {
+      this.sidenav.close();
+    } else {
+      this.sidenav.open();
+    }
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+  onResize($event) {
+    if ($event.target.innerWidth <= 799) {
+      this.sidenav.close();
+    } else {
+      this.sidenav.open();
+    }
+  }
+
 }
--- Checklist/src/app/app.module.ts
+++ Checklist/src/app/app.module.ts
@@ -1,16 +1,62 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {
+  MdButtonModule,
+  MdIconModule,
+  MdProgressSpinnerModule,
+  MdCheckboxModule,
+  MdCardModule,
+  MdSidenavModule,
+  MdProgressBarModule,
+  MdAutocompleteModule,
+  MdRadioModule,
+  MdInputModule,
+  MdToolbarModule,
+  MdDialogModule,
+  MdTooltipModule
+} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import {AppComponent} from './app.component';
+import 'hammerjs';
+import {environment} from '../environments/environment';
+import { ComponentViewerComponent } from './component-viewer/component-viewer.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentViewerComponent,
+    PageHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdButtonModule,
+    MdIconModule,
+    MdToolbarModule,
+    MdDialogModule,
+    MdRadioModule,
+    MdTooltipModule,
+    MdProgressBarModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    MdSidenavModule,
+    MdProgressSpinnerModule,
+    MdAutocompleteModule,
+    MdCardModule,
+    MdInputModule,
+    MdCheckboxModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
