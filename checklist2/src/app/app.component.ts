5d1759893d98619267c2cc93339046a0797230a5
Nino <nino.zumstein@gmail.com>
2018-01-12 22:38:55 +0100

    basic layout from before, is working again

--- checklist2/src/app/app.component.ts
+++ checklist2/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, OnInit} from '@angular/core';
+// import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+// import { AngularFireAuth } from 'angularfire2/auth';
+// import { Observable } from 'rxjs/Observable';
+// import * as firebase from 'firebase/app';
+import {MatSidenav} from '@angular/material';
+import { AuthService } from './core/auth.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('start') sidenav: MatSidenav;
+
+
+  constructor(public auth: AuthService) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  onResize($event) {
+    if ($event.target.innerWidth <= 799) {
+      this.sidenav.close();
+    } else {
+      this.sidenav.open();
+    }
+  }
+
 }
