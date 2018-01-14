4961ca8368b48c12052e2d6451e51e5926085dd1
Nino <nino.zumstein@gmail.com>
2018-01-14 17:16:30 +0100

    deleted old usless projects

--- Checklist/src/app/models/Customer.ts
+++
@@ -1,9 +0,0 @@
-// Interface for the customers collection on firestore
-export interface Customer {
-    id?: string;
-    name?: string;
-    domain?: string;
-    date_created?: number;
-    date_last_edited?: number;
-    answers?: {};
-}
--- Checklist/src/app/models/Question.ts
+++
@@ -1,7 +0,0 @@
-// Interface for the question collection on firestore
-export interface Question {
-    id?: string;
-    name?: string;
-    ref?: {refDB?: string, refPath?: string };
-}
-
